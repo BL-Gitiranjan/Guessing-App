65a4823eb86265bec399e390c0563ac8b1faf913
BL-Gitiranjan <gitiranjan.dalei@bridgelabz.com>
2026-02-06 12:05:24 +0530

    [Gitiranjan] modified in main GuessingApp file in uc2-Game-submission task

--- src/GuessingApp.java
+++ src/GuessingApp.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 public class GuessingApp {
     public static void main(String[] args) {
-        GameConfig gameConfig=new GameConfig();
-        gameConfig.showRules();
+        System.out.println("Welcome to guessing app");
+        GameConfig config=new GameConfig();
+        config.showRules();
+        Scanner scanner=new Scanner(System.in);
+        int attempts=0;
+        while(attempts<config.getMaxAttempts())
+        {
+            System.out.println("enter your guess");
+       int guess=scanner.nextInt();
+       attempts++;
+       String result=GuessValidator.validateGuess(guess,config.getTargetNumber());
+            System.out.println(result);
+            if("Correct".equals(result))
+            {
+                break;
+            }
+        }
     }
 }
